79bf12465c9c0e6a75bc97fdb3209c73963070c2
David-Gyavi <dbloosi12@gmail.com>
2021-08-17 22:21:52 +0000

    html pages created with variables

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='blog-homepage'),
+     path('about', views.about, name='blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 def homepage(request):
-    return render(request, 'blog/homepage.html')
+    return render(request, 'blog/homepage.html', {'title': 'Home Django project'})
+
+def about(request):
+    return render(request, 'blog/about.html', {'title':'About Django project'})    
 
 # Create your views here.
