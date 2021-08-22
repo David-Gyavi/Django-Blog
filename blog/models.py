cfe92f7f508879c11be5dec125d8478734311c44
David-Gyavi <dbloosi12@gmail.com>
2021-08-22 14:55:53 +0000

    Django models are created in project

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Musician(models.model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=50)
+    post_created = models.DateTimeField(default=timezone.now())
+
+class Album(models.Model):
+    artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    release_date = models.DateField()
+    num_stars = model.IntegerField()    
