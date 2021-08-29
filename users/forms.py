b1cd29fbd65d6be294772a73739635630a30190d
David-Gyavi <dbloosi12@gmail.com>
2021-08-29 22:03:44 +0000

    Updating navigations bar

--- users/forms.py
+++ users/forms.py
@@ -3,10 +3,18 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class UserRegistrationForm(UserCreationForm):
-    first_name = forms.CharField(max_length=100, help_text='100 characters max.')
-    last_name = forms.CharField(max_length=100, help_text='100 characters max.')
-    email = forms.CharField(help_text='A valid email address, please.')
+    first_name = forms.CharField(max_length=100, help_text='100 characters max', required=False)
+    last_name = forms.CharField(max_length=100, help_text='100 characters max', required=False)
+    email = forms.CharField(required=False, help_text='A valid email address, please.')
+    password1 = forms.CharField(label='Enter password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
+    
+
 
     class Meta:
         model = User
+        User.username = forms.CharField(max_length=100, required=False)
         fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+        help_texts = {
+            'username' : None,
+        }
--- users/views.py
+++ users/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . forms import UserRegistrationForm
 
 
 def register(request):
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            return redirect('blog-homepage')
     else:
         form = UserRegistrationForm()
     return render(request, 'users/register.html', {'form': form})
