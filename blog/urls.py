3f1611c38da691fbff2dfe5c8f6dd694be8a1a24
David-Gyavi <dbloosi12@gmail.com>
2021-09-08 21:18:44 +0000

    Hyperlinks in detailed view are added

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.HomeListView.as_view(), name='blog-homepage'),
     path('about', views.AboutTemplateView.as_view(), name='blog-about'),
+    path('post/<int:pk>', views.PostDetailView.as_view(), name='blog-post-detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from . models import Blog
-from django.views.generic import ListView, TemplateView
+from django.views.generic import ListView, TemplateView, DetailView
 
 
 class HomeListView(ListView):
     model = Blog
     context_object_name = 'blogs'
     extra_context = {'title': 'Home Django project'}
-    template_name = 'blog/homepage.html'      
+    ordering =["-post_created"]
+    template_name = 'blog/homepage.html'
+
+class PostDetailView(DetailView): 
+    pass         
 
 
 class AboutTemplateView(TemplateView):
