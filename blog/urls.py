b06cb543737e0b4a48e1b4067385970749b45a28
David-Gyavi <dbloosi12@gmail.com>
2021-09-08 19:53:55 +0000

    Django class based view is created

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage, name='blog-homepage'),
+    path('', views.HomeListView.as_view(), name='blog-homepage'),
     path('about', views.about, name='blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from . models import Blog
+from django.views.generic import ListView
 
-def homepage(request):
-    blogs = Blog.objects.all()
-    content = {'title': 'Home Django project', 'blog': blogs}
+#def homepage(request):
+#    blogs = Blog.objects.all()
+#    content = {'title': 'Home Django project', 'blog': blogs}
 
 
-    return render(request, 'blog/homepage.html', content)
+#    return render(request, 'blog/homepage.html', content)
+
+class HomeListView(ListView):
+    model = Blog
+    context_object_name = 'blogs'
+    extra_context = {'title': 'Home Django project'}
+    template_name = 'blog/homepage.html'      
 
 def about(request):
-    return render(request, 'blog/about.html', {'title': 'About Django project'})    
+    return render(request, 'blog/about.html', 
+                 {'title': 'About Django project'})    
 
 
