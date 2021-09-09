b42536d6a73640f7c84b77b848309f8c6d0ed864
David-Gyavi <dbloosi12@gmail.com>
2021-09-09 23:23:44 +0000

    intial commit

--- blog_winkel/settings.py
+++ blog_winkel/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -143,3 +147,11 @@ LOGIN_URL = 'users-login'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+#SMTP settings
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_USE_TLS=True
+EMAIL_PORT=587
+EMAIL_HOST_USER='vken8936@gmail.com'
+EMAIL_HOST_PASSWORD='Kellyd12'
--- blog_winkel/urls.py
+++ blog_winkel/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
                 path('admin/', admin.site.urls),
                 path('', include('blog.urls')),
                 path('users/', include('users.urls')),
-                path('accounts/', include('django.contrib.auth.urls'))
+                #path('accounts/', include('django.contrib.auth.urls')),
              # Used during the development, not recommended in production
            ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
